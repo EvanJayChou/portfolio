61cfe896e7cf5923952e9340f891c2ed526e2be7
EvanJayChou <evan.chou@live.com>
2025-06-07 23:13:26 -0700

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
 }
 
 export default nextConfig
